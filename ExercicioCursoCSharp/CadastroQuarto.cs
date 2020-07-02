743fb53315b4098363d8ff44f5c74032e09d201f
Juan Ormelli <juan_ormelli@hotmail.com>
2020-07-02 20:11:09 -0300

    Exercicio Treinamento Inicio com Csharp

---
+++ ExercicioCursoCSharp/CadastroQuarto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioCursoCSharp
+{
+    class CadastroQuarto
+    {
+        public string Nome { get; set; }
+        public int NumeroQuarto { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return "Nome, " + Nome + ", Email, " + Email + ", Quarto Alugado, " + NumeroQuarto;
+        }
+    }
+}
---
+++ ExercicioCursoCSharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExercicioCursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CadastroQuarto[] cadastro = new CadastroQuarto[10];
+            
+            Console.WriteLine("Digite o Numero de quartos a serem alugados");
+            double n = double.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite o Nome ");
+                string Nome = Console.ReadLine();
+                Console.WriteLine("Digite o Numero do quarto");
+                int NumQuart = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o e-mail");
+                string email = Console.ReadLine();
+                cadastro[NumQuart] = new CadastroQuarto { Nome = Nome, NumeroQuarto = NumQuart, Email = email };
+
+            }
+            Console.WriteLine("Situação dos quartos");
+            for(int i = 0; i < 10; i++)
+            {
+               if (cadastro[i] == null)
+                {
+                    Console.WriteLine((i)+":  Disponivel"); 
+                }
+                else
+                {
+                    Console.WriteLine((i)+"  "+cadastro[i]);
+                }
+            }
+        }
+    }
+}
